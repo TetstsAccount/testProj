174c831dfe70f87e444871004900a8c3e0d3df27
NiKoTiN1 <56438316+NiKoTiN1@users.noreply.github.com>
2021-05-18 23:55:06 +0300

    self
    aaaa

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 5;
+            Console.WriteLine(a);
         }
     }
 }
